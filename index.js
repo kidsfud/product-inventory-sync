71e713084194b483a7305e8857ceda2f3ecb0a92
Teja <kidsfud2@gmail.com>
2025-04-25 16:26:15 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const { updateWooCommerceInventory } = require("./woocommerce-api");
 
 const app = express();
